52fb0611c1c088d11b862558904b8ddf2b8ad0b2
Justin <jcdiner@gmail.com>
2023-02-20 16:44:07 -0500

    sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,13 @@ class SessionsController < ApplicationController
 
     def create
         @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+
+		if @user
+			login(@user)
+			redirect_to cats_url
+		else
+			render :new 
+		end
         
     end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
     def new
-        route :new
+        render :new
     end
 
     def create
         @user = User.new(user_params)
         if @user.save 
-            route_to cats_url
+            redirect_to cats_url
         else
-            route :new 
+            render :new 
         end
     end
 
