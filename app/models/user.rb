1453efb03ad6f30c2d3a43ec105913923240525b
Yong <yong.lin13@gmail>
2023-02-20 15:34:09 -0500

    find by credentials

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
 		self.session_token ||= generate_session_token 
 	end
 
-	def generate_session_token
+	def generate_unique_session_token
 		token = SecureRandom::urlsafe_base64
 		while User.exists?(session_token: token)
 			token = SecureRandom::urlsafe_base64
@@ -18,4 +18,20 @@ class User < ApplicationRecord
 		self.password_digest = Bcrypt::Password.create(password)
 		@password = password
 	end
+
+	def is_password?(password)
+		bcrypt_obj = BCrypt::Password.new(self.password_digest)
+		bcrypt_obj.is_password?(password)
+	end
+
+	def self.find_by_credentials(username, password)
+		user = User.find_by(username: username)
+		if user && user.is_password?(password)
+			return user 
+		else 
+			nil
+		end
+	end
+
+
 end
