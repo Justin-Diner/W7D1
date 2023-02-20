01965c19c7f3e06e571afe015021a31bd5c998ab
Justin <jcdiner@gmail.com>
2023-02-20 15:12:48 -0500

    continuing user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,21 @@
 class User < ApplicationRecord
+	before_validation :ensure_session_token 
+	validates :session_token, uniqueness: true 
 
+	def ensure_session_token 
+		self.session_token ||= generate_session_token 
+	end
 
+	def generate_session_token
+		token = SecureRandom::urlsafe_base64
+		while User.exists?(session_token: token)
+			token = SecureRandom::urlsafe_base64
+		end
+		token 
+	end
+
+	def password=(password)
+		self.password_digest = Bcrypt::Password.create(password)
+		@password = password
+	end
 end
