f6ec7694d99318ba1dd9ac8040a4f38c1bd92b09
Justin <jcdiner@gmail.com>
2023-02-20 15:46:50 -0500

    reset session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,11 @@
 class User < ApplicationRecord
 	before_validation :ensure_session_token 
-	validates :session_token, uniqueness: true 
+	validates :session_token, :username, uniqueness: true, presence: true 
+	validates :password, length: {minimum: 6}, allow_nil: true 
+	validates :password_digest, presence: true 
 
 	def ensure_session_token 
-		self.session_token ||= generate_session_token 
-	end
-
-	def generate_unique_session_token
-		token = SecureRandom::urlsafe_base64
-		while User.exists?(session_token: token)
-			token = SecureRandom::urlsafe_base64
-		end
-		token 
+		self.session_token ||= generate_unique_session_token 
 	end
 
 	def password=(password)
@@ -33,5 +27,19 @@ class User < ApplicationRecord
 		end
 	end
 
+	def reset_session_token! 
+		self.session_token = generate_unique_session_token
+		self.save!
+		self.session_token
+	end
+
+	private
+	def generate_unique_session_token
+		token = SecureRandom::urlsafe_base64
+		while User.exists?(session_token: token)
+			token = SecureRandom::urlsafe_base64
+		end
+		token 
+	end
 
 end
